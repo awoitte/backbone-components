3b7399833d04c29c5208c99587261fc691c31efe
Alicia Woitte <alicia.woitte@confirmit.com>
2014-11-10 09:14:12 -0800

    Adding immutable repeater. A version of the repeater that never recycles components.

---
+++ js/Components/Basic/immutable-repeater.js
@@ -0,0 +1,105 @@
+! function() {
+  'use strict';
+
+  define([
+    'Components/Base/ComponentView',
+    'backbone'
+  ], function(ComponentView) {
+
+    /**
+     * The repeater component creates a component for each model in a collection (its model property)
+     * modelComponent option is the constructor for the component to make
+     * modelOptions is the options to pass to each new component
+     * @constructor
+     */
+    var RepeaterComponent = ComponentView.extend({
+
+      initialize: function() {
+        this.options.template = "";
+        this.components = {};
+        this.components[this.getTagName()] = [];
+
+        this.library = [];
+
+        RepeaterComponent.__super__.initialize.apply(this, arguments);
+      },
+
+      render: function() {
+        this.updateComponents();
+        RepeaterComponent.__super__.render.apply(this, arguments);
+        this.cleanupLibrary();
+      },
+
+      updateComponents: function() {
+
+        var componentsArray = [];
+
+        //generate component list
+        for (var i = 0; i < this.model.length; i++) {
+          var model = this.getModelAt(i),
+            component = this.getComponentFromLibrary(model);
+          if (!component) component = this.addModelToLibrary(model);
+
+          componentsArray.push(component);
+        }
+
+        this.components[this.getTagName()] = componentsArray;
+
+      },
+
+      addModelToLibrary: function(model) {
+        var newComponent = this.makeNewComponent({
+          model: model
+        });
+
+        this.library.push({
+          model: model,
+          component: newComponent
+        });
+
+        return newComponent;
+      },
+
+      getComponentFromLibrary: function(model) {
+        var retModel = false;
+        _.each(this.library, function(map) {
+          if (map.model === model) retModel = map.component;
+        });
+        return retModel;
+      },
+
+      getModelAt: function(i) {
+        //using a backbone collection V.S. an array
+        return this.model.models ? this.model.models[i] : this.model[i];
+      },
+
+      getModels: function() {
+        return this.model.models ? this.model.models : this.model;
+      },
+
+      makeNewComponent: function(options) {
+        return new this.options.modelComponent(
+          _.extend(
+            //the new component should use the correct model
+            options,
+            //pass it any extra options
+            this.options.modelOptions
+          )
+        );
+      },
+
+      cleanupLibrary: function() {
+        var models = this.getModels(),
+          zombieMaps = [];
+
+        _.each(this.library, function(map) {
+          if (!_.contains(models, map.model)) zombieMaps.push(map);
+        });
+
+        this.library = _.without(this.library, zombieMaps);
+      }
+    });
+
+    return RepeaterComponent;
+  });
+}();
