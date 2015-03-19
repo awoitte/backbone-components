be72c9c6f617c52595c0b6c887289c4821714705
Alicia Woitte <alicia.woitte@confirmit.com>
2015-03-19 15:38:00 -0700

    Fixed a bug with refocusing text input component. Instead I've opted to update silently

--- js/Components/Basic/textinput.js
+++ js/Components/Basic/textinput.js
@@ -1,51 +1,54 @@
-! function() {
+!function() {
   'use strict';
 
   define([
     'Components/Base/Component'
   ], function(Component) {
 
-    /**
-     * A simple text input component.
-     * has callbacks for both keup and change events
-     * @constructor
-     */
-    var TextInputComponent = Component.extend({
-      tagName: "input",
+      /**
+       * A simple text input component.
+       * has callbacks for both keup and change events
+       * @constructor
+       */
+      var TextInputComponent = Component.extend({
+        tagName: "input",
 
-      events: {
-        "change": "change",
-        "keyup": "keyup"
-      },
+        events: {
+          "change": "change",
+          "keyup": "keyup"
+        },
 
-      initialize: function() {
-        TextInputComponent.__super__.initialize.apply(this, arguments);
-      },
+        initialize: function() {
+          TextInputComponent.__super__.initialize.apply(this, arguments);
+        },
 
-      change: function(e) {
-        var val = e.target.value;
+        change: function(e) {
+          var val = e.target.value;
 
-        if(!this.options.manualUpdate && this.model.set) this.model.set(this.options.property, val);
+          if (!this.options.manualUpdate && this.model.set) this.model.set(this.options.property, val, {
+              silent: !this.options.setLoud //fix bug where a parent component would share the same model and setting the property would loose focus on typing
+            });
 
-        if (this.options.onChange) this.options.onChange(val, e);
-      },
+          if (this.options.onChange) this.options.onChange(val, e);
+        },
 
-      keyup: function (e) {
-        var val = e.target.value;
+        keyup: function(e) {
+          var val = e.target.value;
 
-        if(!this.options.manualUpdateKeyup && this.model.set) this.model.set(this.options.property, val);
-        if(!this.options.stopRefocus) this.$el.focus();//fix bug where a parent component would share the same model and setting the property would loose focus on typing
+          if (!this.options.manualUpdateKeyup && this.model.set) this.model.set(this.options.property, val, {
+              silent: !this.options.setLoud //fix bug where a parent component would share the same model and setting the property would loose focus on typing
+            });
 
-        if (this.options.onKeyup) this.options.onKeyup(val, e);
-      },
+          if (this.options.onKeyup) this.options.onKeyup(val, e);
+        },
 
-      render: function () {
-        TextInputComponent.__super__.render.apply(this, arguments);
+        render: function() {
+          TextInputComponent.__super__.render.apply(this, arguments);
 
-        this.$el.val(this.model.get(this.options.property));
-      }
+          this.$el.val(this.model.get(this.options.property));
+        }
+      });
+
+      return TextInputComponent;
     });
-
-    return TextInputComponent;
-  });
 }();
